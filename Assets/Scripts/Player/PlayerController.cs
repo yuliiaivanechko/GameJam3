d00a7f84457a578c376e7116ea259899ef8fcf68
Maksym Protsyk <maksym.protsyk@gameloft.com>
2023-12-27 00:20:49 +0200

    + added some animation and moved the input actions asset for player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,26 +9,62 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _moveSpeed;
 
-    private PlayerInput _playerInput;
+    [SerializeField]
+    private float _jumpSpeed;
+
+    [SerializeField]
+    private int _additionalJumps;
+
+    [SerializeField]
+    private Collider2D _bottomCollider;
+
+    [SerializeField]
+    private LayerMask _groundLayer;
+
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+    private static readonly int LookingLeft = Animator.StringToHash("LookingLeft");
+
+
     private Rigidbody2D _rigidbody;
     private float _horizontalInput;
+    private int _jumpsLeft;
+    private bool _isOnGround;
+    private Animator _animator;
+
     // Start is called before the first frame update
     private void Start()
     {
-        _playerInput = GetComponent<PlayerInput>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _horizontalInput = 0.0f;
+        _jumpsLeft = _additionalJumps;
+        _isOnGround = _bottomCollider.IsTouchingLayers(_groundLayer);
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(_moveSpeed * _horizontalInput, 0.0f);
+        _rigidbody.velocity = new Vector2(_moveSpeed * _horizontalInput, _rigidbody.velocity.y);
+        bool isMoving = _rigidbody.velocity.x != 0;
+        _animator.SetBool(IsMoving, isMoving);
+        if (isMoving)
+        {
+            _animator.SetBool(LookingLeft, _rigidbody.velocity.x < 0);
+        }
+        UpdateJumpState();
     }
 
     // Update is called once per frame
     private void Update()
     {
         
+    }
+    private void UpdateJumpState()
+    {
+        _isOnGround = _bottomCollider.IsTouchingLayers(_groundLayer);
+        if (_isOnGround)
+        {
+            _jumpsLeft = _additionalJumps;
+        }
     }
 
     private void OnMove(InputValue value)
@@ -38,6 +74,21 @@ public class PlayerController : MonoBehaviour
 
     private void OnJump()
     {
+        if (!_isOnGround)
+        {
+            if (_jumpsLeft <= 0)
+            {
+                return;
+            }
+            else
+            {
+                _jumpsLeft--;
+            }
+        }
+        
 
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpSpeed);
     }
+
+
 }
