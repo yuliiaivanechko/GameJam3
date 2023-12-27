3b905744dd802121c45eee1522d1fc98e807c9c3
Maksym Protsyk <maksym.protsyk@gameloft.com>
2023-12-27 19:21:26 +0200

    * fixed camera position after spawning

--- Assets/Scripts/SavePoints/SpawnPointsManager.cs
+++ Assets/Scripts/SavePoints/SpawnPointsManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,6 +19,10 @@ public class SpawnPointsManager : MonoBehaviour, IDataPersistance
     [SerializeField]
     private float _YspawnDelta;
 
+    private void Start()
+    {
+    }
+
     public void LoadData(GameData data, string prevScene)
     {
         int _sceneIndex = _previousScenes.FindIndex((name) => name == prevScene);
@@ -25,7 +30,10 @@ public class SpawnPointsManager : MonoBehaviour, IDataPersistance
         {
             Vector3 pos = _spawnPoints[_sceneIndex].position;
             pos.y += _YspawnDelta;
-            Instantiate(_playerPrefab, pos, _spawnPoints[_sceneIndex].rotation);
+            GameObject obj = Instantiate(_playerPrefab, pos, _spawnPoints[_sceneIndex].rotation);
+            var virtualCamera = FindAnyObjectByType<CinemachineVirtualCamera>();
+            virtualCamera.Follow = obj.transform;
+
         }
     }
     public void SaveData(ref GameData data)
