4099e984f531c9b4c18e962a44e77f4821c0ae80
yuliia_ivanechko <yuliia.ivanechko@gameloft.com>
2023-12-28 15:34:55 +0200

    * updated hammer movement script

--- Assets/Scripts/Enemy/HammerMovement.cs
+++ Assets/Scripts/Enemy/HammerMovement.cs
@@ -2,27 +2,23 @@ using UnityEngine;
 
 public class HammerMovement : MonoBehaviour
 {
-    [SerializeField] float delayInSeconds = 2f; // Adjust this value as needed
+    [SerializeField] float delayInSeconds = 2f;
     private Animator hammerAnimator;
 
     void Start()
     {
-        // Get the Animator component attached to the same GameObject
         hammerAnimator = GetComponent<Animator>();
 
-        // Call a method to start the delay before triggering the animation
         StartAnimationDelay();
     }
 
     void StartAnimationDelay()
     {
-        // Invoke the method to trigger the animation after the specified delay
         Invoke("TriggerHitAnimation", delayInSeconds);
     }
 
     void TriggerHitAnimation()
     {
-        // Set the "Hit" trigger for the animation
         if (hammerAnimator != null)
         {
             hammerAnimator.SetTrigger("Hit");
