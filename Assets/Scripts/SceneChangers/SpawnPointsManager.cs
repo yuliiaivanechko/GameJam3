67dfa1867b13f9b6dc3dd880d4c1e10b9990a698
Maksym Protsyk <maksym.protsyk@gameloft.com>
2023-12-28 18:35:55 +0200

    + added default spawn point to spawn point manager

--- Assets/Scripts/SceneChangers/SpawnPointsManager.cs
+++ Assets/Scripts/SceneChangers/SpawnPointsManager.cs
@@ -14,6 +14,9 @@ public class SpawnPointsManager : MonoBehaviour, IDataPersistance
     private List<string> _previousScenes;
 
     [SerializeField]
+    private Transform _defaultSpawnPoint;
+
+    [SerializeField]
     private GameObject _playerPrefab;
 
     [SerializeField]
@@ -26,15 +29,20 @@ public class SpawnPointsManager : MonoBehaviour, IDataPersistance
     public void LoadData(GameData data, string prevScene)
     {
         int _sceneIndex = _previousScenes.FindIndex((name) => name == prevScene);
+        Vector3 pos;
         if (_sceneIndex != -1)
         {
-            Vector3 pos = _spawnPoints[_sceneIndex].position;
+            pos = _spawnPoints[_sceneIndex].position;
             pos.y += _YspawnDelta;
-            GameObject obj = Instantiate(_playerPrefab, pos, _spawnPoints[_sceneIndex].rotation);
-            var virtualCamera = FindAnyObjectByType<CinemachineVirtualCamera>();
-            virtualCamera.Follow = obj.transform;
-
         }
+        else
+        {
+            pos = _defaultSpawnPoint?.position ?? new Vector3(0.0f, 0.0f);
+        }
+
+        GameObject obj = Instantiate(_playerPrefab, pos, _spawnPoints[_sceneIndex].rotation);
+        var virtualCamera = FindAnyObjectByType<CinemachineVirtualCamera>();
+        virtualCamera.Follow = obj.transform;
     }
     public void SaveData(ref GameData data)
     {
